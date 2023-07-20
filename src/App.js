2f24a5f0d310425b595e992ff46208e27c28c7c5
Sudharsan B <25sudharsan27@gmail.com>
2023-07-20 13:24:16 +0530

    effects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import {useState, useEffect} from "react";
+
+
+
 
 function App() {
+  const [emotion,setEmotion]=useState("happy")
+  
+  useEffect(()=>{
+    console.log("It's ",emotion,'right now');
+  },[emotion])
+
   return (
     <div className="App">
-      <h1>Hello from react</h1>
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={()=> {
+        setEmotion("excited")
+      }}>Excited</button>
+      <button onClick={()=> {
+        setEmotion("sad")
+        }}>Sad</button>
     </div>
   );
 }
