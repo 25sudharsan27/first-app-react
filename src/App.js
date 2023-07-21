f3a312978a60da38c671f80b1580aec0da569ca9
Sudharsan B <25sudharsan27@gmail.com>
2023-07-21 17:42:26 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 import './App.css';
 import {useRef,useState,useEffect} from "react";
+import {Link} from "react-router-dom";
 
 
-
-function GithubUser({name}){
+function Home(){
   return (
     <div>
-      <h1>{name}</h1>
+    <nav>
+      <Link to ="/about">About</Link>
+      <Link to ="/contact">Contact</Link>
+    </nav>
+    <h1>My Website</h1>
     </div>
   )
 }
 
-function App() {
-  const [data,setData]=useState(null);
-  useEffect(()=>{
-    fetch("https://api.github.com/users/25sudharsan27").then((response)=>response.json()).then(setData);
-  },[])
-  if (data)
-    return  <GithubUser name={data.name} />;
-    
+export function About(){
+  return (
+    <div>
+      <nav>
+        <Link  to ="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <h1>About us</h1>
+    </div>
+  )
+}
 
-    
+export function Contact(){
+  return (
+    <div>
+      <nav>
+        <Link  to ="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    <h1>Contact us</h1>
+    </div>
+  )
+}
+
+export function App() {
+  return <h1>My Website</h1>
 
 }
 
-export default App;
+export default Home;
+
+
